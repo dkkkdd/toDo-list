257b13c31b6891993c4088498a19ae0a61c34aed
dkkkdd <fruffr@gmail.com>
2025-10-30 14:57:47 +0200

    settings

---
+++ webpack.common.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+
+  entry: "./src/index.js",
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    assetModuleFilename: "images/[name][ext]",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name][ext]",
+        },
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name][ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: './src/img/favicon.ico',
+
+    }),
+    new MiniCssExtractPlugin({
+      filename: "style.css",
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/data", to: "data" },
+      ],
+    }),
+  ],
+
+};
